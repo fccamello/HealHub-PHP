8d53dd1b9d476f1cd60ecd46243ad9fab40f50cb
fccamello <camellofriamae@gmail.com>
2024-04-04 09:03:54 +0800

    April 4

---
+++ php/about-us.php
@@ -0,0 +1,53 @@
+<?php    
+    include '../php/connect.php';
+    //include 'readrecords.php';   
+    require_once '../includes/header.php'; 
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/about-us.css">
+
+</head>
+<body>
+
+
+      <hr>
+
+      <div id="container">
+
+        <div class="first-div">
+            
+          <div id="about-us-text">
+            ABOUT US
+          </div>
+                
+
+                <div class="about-us-content">
+
+                  Welcome to our advanced medical records database system, where precision meets care. With a commitment to confidentiality and security, we seamlessly organize and manage the health histories of patients. Our user-friendly interface ensures efficient access to vital medical information, promoting streamlined healthcare delivery. Powered by cutting-edge technology, our system adheres to the highest standards of data protection, providing a secure repository for patient records. Experience the future of healthcare data management with our robust and reliable database system, dedicated to revolutionizing the way medical information is handled. Your patients' well-being is our top priority.
+
+                </div>
+
+        </div>
+
+  
+
+        <footer class="page-footer">
+          <h2>Fria Mae Camello</h2>
+          <h4>BSCS - 2</h4>
+      </footer>
+
+
+      </div>
+
+
+
+    
+</body>
+</html> 
---
+++ php/contact-us.php
@@ -0,0 +1,64 @@
+<?php    
+    include '../php/connect.php';
+    //include 'readrecords.php';   
+    require_once '../includes/header.php'; 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/about-us.css">
+
+</head>
+<body>
+    
+
+      <hr>
+    
+      <div id="container">
+
+        <div class="first-div" style="background-color: none">
+            
+            <div id ="contact-us-text">
+                CONTACT US
+
+                 </div>
+
+                <div id="about-us-content-1">
+                    <br>
+                        JOSHUA NAPINAS <br> <br>
+
+                        ☎ 09123456789
+                        <br>
+                        📧 joshua.napinas@cit.edu
+                        <br> 📍 Dalaguete, Cebu 
+                </div>
+
+                <div id="about-us-content-2">
+                    <br>
+                    FRIA MAE CAMELLO <br> <br>
+                    ☎ 09123456789
+                    <br>
+                    📧friamae.camello@cit.edu
+                    <br> 📍 Cebu City, Cebu 
+            </div>
+
+        </div>
+
+        <footer class="page-footer">
+            <h2>Fria Mae Camello</h2>
+            <h4>BSCS - 2</h4>
+        </footer>
+
+
+      </div>
+
+
+
+</body>
+</html>
